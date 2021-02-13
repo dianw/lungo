6e46304a0b0e3631acd8aa8eb2a8352586d05cf8
Dian Aditya <dianw.aditya@gmail.com>
2021-02-13 18:59:20 +0700

    Cleanup unused code

--- src/main/java/xyz/mainapi/dashboard/core/data/MinioConfigProperties.java
+++ src/main/java/xyz/mainapi/dashboard/core/data/MinioConfigProperties.java
@@ -1,15 +1,7 @@
-/**
- * DANA Indonesia
- * Copyright (c) 2017‐2021 All Rights Reserved.
- */
 package xyz.mainapi.dashboard.core.data;
 
 import org.immutables.value.Value;
 
-/**
- * @author Dian Aditya
- * @version $Id: MinioConfigProperties.java, v 0.1 2021‐02‐11 00.08 Dian Aditya Exp $$
- */
 @Value.Modifiable
 public interface MinioConfigProperties {
     String getEndpoint();
