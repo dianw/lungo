8eefaf5bef5bea1c0bb88c73ca24a252b8821090
Dian Aditya <dianw.aditya@gmail.com>
2021-02-22 22:40:44 +0700

    Enabled cross-origin resource sharing

--- src/main/java/xyz/mainapi/dashboard/core/security/WebConfig.java
+++ src/main/java/xyz/mainapi/dashboard/core/security/WebConfig.java
@@ -2,8 +2,18 @@ package xyz.mainapi.dashboard.core.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableSpringDataWebSupport
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH")
+            .allowCredentials(true)
+            .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
+    }
 }
--- src/main/java/xyz/mainapi/dashboard/core/security/WebSecurityConfig.java
+++ src/main/java/xyz/mainapi/dashboard/core/security/WebSecurityConfig.java
@@ -1,17 +1,11 @@
 package xyz.mainapi.dashboard.core.security;
 
-import java.util.Arrays;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -19,26 +13,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
-            .and()
+        http.cors().and()
             .authorizeRequests(authorizeRequests -> authorizeRequests
                 .antMatchers("/api/**").authenticated()
+                .antMatchers("/actuator/health").permitAll()
                 .antMatchers("/**").authenticated()
             )
             .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
             .csrf(CsrfConfigurer::disable);
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOriginPattern("*");
-        corsConfiguration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-
-        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
-        configurationSource.registerCorsConfiguration("/**", corsConfiguration);
-        return configurationSource;
     }
 }
